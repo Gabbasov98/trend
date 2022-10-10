cb249c0c3986fc292cf254cca7c800c1efb1e325
Marsel Gabbasov <marsel13_98@mail.ru>
2022-10-10 07:12:36 +0500

    animate

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -33,3 +33,22 @@ $(".form-group textarea").change(function() {
         $(this).removeClass("input-active")
     }
 })
+
+
+function fadeIn() {
+    $(".animate-opacity").each(function(index, el) {
+        let wt = $(window).scrollTop();
+        let wh = $(window).height();
+        let et = $(el).offset().top;
+        let eh = $(el).outerHeight();
+        let dh = $(document).height();
+        if (wt + wh >= et || wh + wt == dh || eh + et < wh) {
+            $(el).addClass("animate-opacity--active")
+        }
+    });
+}
+
+fadeIn()
+$(window).scroll(function() {
+    fadeIn()
+});
